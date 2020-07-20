9c165e4f6ae48a08a9a6e8522d9b078b664f198e
marsha <marshaannon@gmail.com>
2020-07-20 10:46:25 -0400

    changed relationship to accommodate has many through

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_many :topics
+    has_many :goals, through: :topics, dependent: :delete_all
+    
     has_secure_password
 
     validates_presence_of :email
