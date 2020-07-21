fa0205f12a8aec0da970a2ab6e73949705e5d5c6
marsha <marshaannon@gmail.com>
2020-07-21 10:50:14 -0400

    added accepts nested attributes for

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < ApplicationRecord
     belongs_to :goal
     belongs_to :user
+
+    validates :name, presence: true, uniqueness: true
+
+    has_many_nested_attributes_for :goals
+
 end
