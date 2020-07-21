586f68f65e23f668ad4798721250bfb9f77c7955
marsha <marshaannon@gmail.com>
2020-07-21 10:47:57 -0400

    added detroy action and strong params

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,74 +1,49 @@
 class GoalsController < ApplicationController
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
-  # GET /goals
-  # GET /goals.json
-  def index
-    @goals = Goal.all
-  end
+    def index
+      @goals = Goal.all
+      render json: @goals.as_json
+    end
 
-  # GET /goals/1
-  # GET /goals/1.json
   def show
+    @goal = Goal.find_by_id(params[:id])
+    render json: @goal.as_json
   end
 
-  # GET /goals/new
-  def new
-    @goal = Goal.new
-  end
-
-  # GET /goals/1/edit
-  def edit
-  end
-
-  # POST /goals
-  # POST /goals.json
   def create
     @goal = Goal.new(goal_params)
-
-    respond_to do |format|
-      if @goal.save
-        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
-        format.json { render :show, status: :created, location: @goal }
-      else
-        format.html { render :new }
-        format.json { render json: @goal.errors, status: :unprocessable_entity }
-      end
+    if @goal.save
+      render json: @goal.as_json
+    else
+      render json: @goal.errors, status: :unprocessable_entity
     end
-  end
+  end  
 
-  # PATCH/PUT /goals/1
-  # PATCH/PUT /goals/1.json
   def update
-    respond_to do |format|
-      if @goal.update(goal_params)
-        format.html { redirect_to @goal, notice: 'Goal was successfully updated.' }
-        format.json { render :show, status: :ok, location: @goal }
-      else
-        format.html { render :edit }
-        format.json { render json: @goal.errors, status: :unprocessable_entity }
-      end
+    if @goal.update(goal_params)
+      render json: @goal.as_json
+    else
+      render json: @goal.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /goals/1
-  # DELETE /goals/1.json
   def destroy
     @goal.destroy
-    respond_to do |format|
-      format.html { redirect_to goals_url, notice: 'Goal was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_goal
-      @goal = Goal.find(params[:id])
+      @goal = Goal.find_by_id(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def goal_params
-      params.fetch(:goal, {})
+      params.require(:goal).permit(
+        :id, 
+        :goal_name,
+        :goal_description,
+        :goal_timeline
+       )
     end
 end
